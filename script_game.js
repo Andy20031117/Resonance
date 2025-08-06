96a5e9745ae665fa53d8fbbd53631cb735d87035
Andy20031117 <a111070029@mail.shu.edu.tw>
2025-08-07 00:39:14 +0800

    遊戲區塊初步建立

---
+++ script_game.js
@@ -0,0 +1,7 @@
+document.querySelector(".start-button").addEventListener("click", () => {
+  alert("🚀 準備進入旅程...（此處未來可接關卡）");
+});
+
+document.querySelector(".about-button").addEventListener("click", () => {
+  alert("🎮 『記憶追流』是一場穿越純真與記憶的像素跑酷，\n請選擇角色，一起踏上旅程！");
+});
